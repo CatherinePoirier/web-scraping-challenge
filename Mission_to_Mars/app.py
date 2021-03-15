c08353f63b464f1b96495f23c70ffcc409b51a9a
@chhpoirier <catherinepoirier@comcast.net>
2021-03-15 17:06:24 -0600

    got titles using .content

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,36 +1,24 @@
-from flask import Flask, render_template
-
-# Import our pymongo library, which lets us connect our Flask app to our Mongo database.
+from flask import Flask, render_template, redirect
 import pymongo
-
-# Create an instance of our Flask app.
 app = Flask(__name__)
 
 # Create connection variable
 conn = 'mongodb://localhost:27017'
-
-# Pass connection to the pymongo instance.
 client = pymongo.MongoClient(conn)
-
-# Connect to a database. Will create one if not already available.
-db = client.team_db
-
-# Drops collection if available to remove duplicates
-db.team.drop()
-
-# Creates a collection in the database and inserts two documents
-
-
 
 # Set route
 @app.route('/')
 def index():
-    # Store the entire team collection in a list
-    teams = list(db.team.find())
-    print(teams)
+    data_from_mongo=client.mars_db.mars.find_one()
+    return render_template('index.html', data_from_flask=data_from_mongo)
+@app.route('/scrape')
+def scrape():
+    print('clicked button')
+    #Call the scrape_pars.py, which will return a dictionary of re
+    #store the dictionalry of results to mongo
+    #client.mars_db.mars.insert(, upsert=True)
+    return redirect('/')
 
-    # Return the template with the teams list passed in
-    return render_template('index.html', teams=teams)
 
 
 if __name__ == "__main__":
