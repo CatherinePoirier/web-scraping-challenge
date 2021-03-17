e6013642c2dcd10251c023fca4e047b42df50202
@chhpoirier <catherinepoirier@comcast.net>
2021-03-17 14:59:40 -0600

    finished jupyter notebook started scrape_mars.py

---
+++ Mission_to_Mars/scrape_mars.py
@@ -0,0 +1,110 @@
+/* Dependencies */
+import requests
+
+from splinter import Browser
+from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
+
+import pymongo
+mongo_conn='mongodb://localhost:27107'
+client=pymongo.MongoClient(mongo_conn)
+
+import pandas as pd
+import time
+
+/* Setup splinter */
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
+
+/* NASA Mars News */
+url = "https://mars.nasa.gov/news/"
+browser.visit(url)
+time.sleep(1)
+html = browser.html
+soup = BeautifulSoup(html, 'html.parser')
+news_titlelong= soup.find_all('div', class_="content_title")
+news_title=news_titlelong[1].text
+news_p= soup.find_all('div', class_="article_teaser_body")
+news_p=(news_p[0].text)
+
+/* JPL Mars Space Images - Featured Image */
+url="https://www.jpl.nasa.gov/images?search=&category=Mars"
+browser.visit(url)
+time.sleep(1)
+button = browser.find_by_css(".BaseImage")
+button.click()
+html = browser.html
+soup = BeautifulSoup(html, 'html.parser')
+featured_image= soup.find_all('img', class_="BaseImage")
+featured_image_url=(featured_image[0]['src'])
+
+/* Mars Facts  */
+url_space = "https://space-facts.com/mars/"
+tables = pd.read_html(url_space)
+df = tables[0]
+html_table = df.to_html('table.html') 
+
+/* Mars Hemispheres */
+url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+browser.visit(url)
+time.sleep(1)
+html=browser.html
+soup = BeautifulSoup(html, 'html.parser') 
+descriptions = soup.find_all('div', class_='description')
+
+h_title = []
+hold=[]
+for desc in descriptions:
+         hold=desc.find('h3')
+         print(hold)
+         h_title.append(hold.contents)
+mars_hem_1=(h_title[0])
+mars_hem_2=(h_title[1])
+mars_hem_3=(h_title[2])
+mars_hem_4=(h_title[3])
+
+button = browser.find_by_css(".thumb")[0]
+button.click()
+html=browser.html
+soup = BeautifulSoup(html, 'html.parser')
+mars_image1= soup.find('img', class_="wide-image").get("src")
+mars_image1=('https://astrogeology.usgs.gov' + mars_image1)
+
+browser.visit(url)  
+time.sleep(1)
+
+button = browser.find_by_css(".thumb")[1]
+button.click()
+html=browser.html
+soup = BeautifulSoup(html, 'html.parser')
+mars_image2= soup.find('img', class_="wide-image").get("src")
+mars_image2=('https://astrogeology.usgs.gov' + mars_image2)
+browser.visit(url) 
+
+button = browser.find_by_css(".thumb")[2]
+button.click()
+
+html=browser.html
+soup = BeautifulSoup(html, 'html.parser')
+mars_image3= soup.find('img', class_="wide-image").get("src")
+mars_image3=('https://astrogeology.usgs.gov' + mars_image3)
+browser.visit(url)  
+
+button = browser.find_by_css(".thumb")[3]
+button.click()
+
+html=browser.html
+soup = BeautifulSoup(html, 'html.parser')
+mars_image4= soup.find('img', class_="wide-image").get("src")
+mars_image4=('https://astrogeology.usgs.gov' + mars_image4)
+
+browser.visit(url)  
+
+hemisphere_image_urls= [
+              {"title": mars_hem_1[0], "img_url": mars_image1},
+              {"title": mars_hem_2[0], "img_url": mars_image2},
+              {"title": mars_hem_3[0], "img_url": mars_image3},
+              {"title": mars_hem_4[0], "img_url": mars_image4}
+            ]
+
+browser.quit()
